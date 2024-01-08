32288b44bd06f5b54a2040455f4a513fedcfc7eb
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-08 14:40:01 -0300

    Category

---
+++ src/Core/Domain/Entity/Traits/MethodsMagicsTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Core\Domain\Entity\Traits;
+
+use Exception;
+
+trait MethodsMagicsTrait
+{
+    /**
+     * @throws Exception
+     */
+    public function __get(string $name)
+    {
+        if ($this->{$name})
+            return $this->{$name};
+
+        $className = get_class($this);
+        throw new Exception("Property {$name} not found in class {$className}");
+    }
+}
