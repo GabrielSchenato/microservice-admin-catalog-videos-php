80555a13a9135a5addf286fa31af6abf70e62031
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-08 19:23:09 -0300

    Category Use Case List

---
+++ src/Core/UseCase/Category/ListCategoryUseCase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Core\UseCase\Category;
+
+use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\UseCase\DTO\Category\CategoryInputDto;
+use Core\UseCase\DTO\Category\CategoryOutputDto;
+
+class ListCategoryUseCase
+{
+    public function __construct(
+        protected CategoryRepositoryInterface $repository
+    )
+    {
+    }
+
+    public function execute(CategoryInputDto $input): CategoryOutputDto
+    {
+        $category = $this->repository->findById($input->id);
+
+        return new CategoryOutputDto(
+            id: $category->id(),
+            name: $category->name,
+            description: $category->description,
+            is_active: $category->isActive
+        );
+    }
+}
--- tests/Unit/UseCase/Category/ListCategoryUseCaseUnitTest.php
+++ tests/Unit/UseCase/Category/ListCategoryUseCaseUnitTest.php
@@ -4,17 +4,17 @@ namespace Tests\Unit\UseCase\Category;
 
 use Core\Domain\Entity\Category;
 use Core\Domain\Repository\CategoryRepositoryInterface;
-use Core\UseCase\Category\CreateCategoryUseCase;
-use Core\UseCase\DTO\Category\CategoryCreateInputDto;
-use Core\UseCase\DTO\Category\CategoryCreateOutputDto;
+use Core\UseCase\Category\ListCategoryUseCase;
+use Core\UseCase\DTO\Category\CategoryInputDto;
+use Core\UseCase\DTO\Category\CategoryOutputDto;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid;
 use stdClass;
 
-class CreateCategoryUseCaseUnitTest extends TestCase
+class ListCategoryUseCaseUnitTest extends TestCase
 {
-    public function testCreateNewCategory()
+    public function testGetById()
     {
         $uuid = Uuid::uuid4()->toString();
         $categoryName = 'New Cat';
@@ -25,16 +25,19 @@ class CreateCategoryUseCaseUnitTest extends TestCase
         $mockEntity->shouldReceive('id')->andReturn($uuid);
 
         $mockRepository = Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
-        $mockRepository->shouldReceive('insert')->andReturn($mockEntity);
+        $mockRepository
+            ->shouldReceive('findById')
+            ->with($uuid)
+            ->andReturn($mockEntity);
 
-        $mockInputDto = Mockery::mock(CategoryCreateInputDto::class, [
-            $categoryName
+        $mockInputDto = Mockery::mock(CategoryInputDto::class, [
+            $uuid
         ]);
 
-        $useCase = new CreateCategoryUseCase($mockRepository);
+        $useCase = new ListCategoryUseCase($mockRepository);
         $response = $useCase->execute($mockInputDto);
 
-        $this->assertInstanceOf(CategoryCreateOutputDto::class, $response);
+        $this->assertInstanceOf(CategoryOutputDto::class, $response);
         $this->assertEquals($categoryName, $response->name);
         $this->assertEquals('', $response->description);
 
@@ -43,12 +46,14 @@ class CreateCategoryUseCaseUnitTest extends TestCase
          */
 
         $spy = Mockery::spy(stdClass::class, CategoryRepositoryInterface::class);
-        $spy->shouldReceive('insert')->andReturn($mockEntity);
+        $spy->shouldReceive('findById')
+            ->with($uuid)
+            ->andReturn($mockEntity);
 
-        $useCase = new CreateCategoryUseCase($spy);
-        $useCase->execute($mockInputDto);
+        $useCase = new ListCategoryUseCase($spy);
+        $response = $useCase->execute($mockInputDto);
 
-        $spy->shouldHaveReceived('insert');
+        $spy->shouldHaveReceived('findById');
     }
 
     protected function tearDown(): void
