2669d3da853bf1b0c50427cfc492865f9523132e
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-09 09:19:10 -0300

    Category Delete Use Case Test

---
+++ tests/Feature/Core/UseCase/Category/DeleteCategoryUseCaseTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Category;
+
+use App\Models\Category as Model;
+use App\Repositories\Eloquent\CategoryEloquentRepository;
+use Core\UseCase\Category\DeleteCategoryUseCase;
+use Core\UseCase\DTO\Category\CategoryInputDto;
+use Tests\TestCase;
+
+class DeleteCategoryUseCaseTest extends TestCase
+{
+    public function testDelete(): void
+    {
+        $categoryDb = Model::factory()->create();
+
+        $repository = new CategoryEloquentRepository(new Model());
+        $useCase = new DeleteCategoryUseCase($repository);
+        $useCase->execute(
+            new CategoryInputDto(
+                id: $categoryDb->id
+            )
+        );
+
+        $this->assertSoftDeleted('categories', [
+            'id' => $categoryDb->id
+        ]);
+    }
+}
