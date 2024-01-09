a72e7404a75d399a86ff2c5e9446dddd6519b22a
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-09 20:43:09 -0300

    Genre Test

---
+++ tests/Unit/Domain/Entity/CategoryEntityUnitTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Unit\Domain\Entity;
+
+use Core\Domain\Entity\CategoryEntity;
+use Core\Domain\Exception\EntityValidationException;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use Throwable;
+
+class CategoryEntityUnitTest extends TestCase
+{
+    public function testAttributes()
+    {
+        $category = new CategoryEntity(
+            name: 'New Cat',
+            description: 'New desc',
+            isActive: true
+        );
+
+        $this->assertNotEmpty($category->id());
+        $this->assertNotEmpty($category->createdAt());
+        $this->assertEquals('New Cat', $category->name);
+        $this->assertEquals('New desc', $category->description);
+        $this->assertTrue($category->isActive);
+    }
+
+    public function testActivated()
+    {
+        $category = new CategoryEntity(
+            name: 'New Cat',
+            isActive: false
+        );
+
+        $this->assertFalse($category->isActive);
+        $category->activate();
+        $this->assertTrue($category->isActive);
+    }
+
+    public function testDisabled()
+    {
+        $category = new CategoryEntity(
+            name: 'New Cat'
+        );
+
+        $this->assertTrue($category->isActive);
+        $category->disabled();
+        $this->assertFalse($category->isActive);
+    }
+
+    public function testUpdate()
+    {
+        $uuid = Uuid::uuid4()->toString();
+        $createdAt = '2024-01-01 12:00:00';
+
+        $category = new CategoryEntity(
+            id: $uuid,
+            name: 'Old Cat',
+            description: 'Old desc',
+            isActive: true,
+            createdAt: $createdAt
+        );
+        $category->update(
+            name: 'New Cat',
+            description: 'New desc'
+        );
+
+        $this->assertEquals($uuid, $category->id());
+        $this->assertEquals($createdAt, $category->createdAt());
+        $this->assertEquals('New Cat', $category->name);
+        $this->assertEquals('New desc', $category->description);
+    }
+
+    public function testExceptionName()
+    {
+        try {
+            new CategoryEntity(
+                name: 'Na',
+                description: 'New desc'
+            );
+
+            $this->fail();
+        } catch (Throwable $th) {
+            $this->assertInstanceOf(EntityValidationException::class, $th);
+        }
+    }
+
+    public function testExceptionDescription()
+    {
+        try {
+            new CategoryEntity(
+                name: 'New Cat',
+                description: random_bytes(300)
+            );
+
+            $this->fail();
+        } catch (Throwable $th) {
+            $this->assertInstanceOf(EntityValidationException::class, $th);
+        }
+    }
+}
---
+++ tests/Unit/Domain/Entity/GenreEntityUnitTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Unit\Domain\Entity;
+
+use Core\Domain\Entity\GenreEntity;
+use Core\Domain\Exception\EntityValidationException;
+use Core\Domain\ValueObject\Uuid;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid as RamseyUuid;
+
+class GenreEntityUnitTest extends TestCase
+{
+
+    public function testAttributesCreate()
+    {
+        $name = 'New Genre';
+        $genre = new GenreEntity(
+            name: $name,
+        );
+
+        $this->assertNotEmpty($genre->id());
+        $this->assertEquals($name, $genre->name);
+        $this->assertTrue($genre->isActive);
+        $this->assertNotEmpty($genre->createdAt());
+    }
+
+    public function testAttributesUpdate()
+    {
+        $uuid = RamseyUuid::uuid4();
+        $name = 'New Genre';
+        $date = date('Y-m-d H:i:s');
+        $genre = new GenreEntity(
+            name: $name,
+            id: new Uuid($uuid),
+            isActive: true,
+            createdAt: new DateTime($date)
+        );
+
+        $this->assertEquals($uuid, $genre->id());
+        $this->assertEquals($name, $genre->name);
+        $this->assertTrue($genre->isActive);
+        $this->assertEquals($date, $genre->createdAt());
+    }
+
+    public function testActivated()
+    {
+        $genre = new GenreEntity(
+            name: 'New Genre',
+            isActive: false
+        );
+
+        $this->assertFalse($genre->isActive);
+        $genre->activate();
+        $this->assertTrue($genre->isActive);
+    }
+
+    public function testDisabled()
+    {
+        $genre = new GenreEntity(
+            name: 'New Genre'
+        );
+
+        $this->assertTrue($genre->isActive);
+        $genre->disabled();
+        $this->assertFalse($genre->isActive);
+    }
+
+    public function testUpdate()
+    {
+        $uuid = new Uuid(RamseyUuid::uuid4()->toString());
+        $createdAt = '2024-01-01 12:00:00';
+
+        $genre = new GenreEntity(
+            name: 'Old Genre',
+            id: $uuid,
+            isActive: true,
+            createdAt: new DateTime($createdAt)
+        );
+        $genre->update(
+            name: 'New Genre'
+        );
+
+        $this->assertEquals($uuid, $genre->id());
+        $this->assertEquals($createdAt, $genre->createdAt());
+        $this->assertEquals('New Genre', $genre->name);
+    }
+
+    public function testExceptionName()
+    {
+        $this->expectException(EntityValidationException::class);
+        new GenreEntity(
+            name: 'Na'
+        );
+    }
+
+}
