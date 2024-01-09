1fc454304a83a4dd75007a1874fbab91ebf89381
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-09 15:46:12 -0300

    Category Controller Feature Test

--- tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
@@ -3,20 +3,28 @@
 namespace Tests\Feature\App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Requests\StoreCategoryRequest;
 use App\Models\Category;
 use App\Repositories\Eloquent\CategoryEloquentRepository;
+use Core\UseCase\Category\CreateCategoryUseCase;
 use Core\UseCase\Category\ListCategoriesUseCase;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Symfony\Component\HttpFoundation\InputBag;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class CategoryControllerTest extends TestCase
 {
     protected CategoryEloquentRepository $repository;
+    protected CategoryController $controller;
 
     protected function setUp(): void
     {
         $this->repository = new CategoryEloquentRepository(new Category());
+        $this->controller = new CategoryController();
 
         parent::setUp();
     }
@@ -24,11 +32,25 @@ class CategoryControllerTest extends TestCase
     public function testIndex(): void
     {
         $useCase = new ListCategoriesUseCase($this->repository);
-
-        $controller = new CategoryController();
-        $response = $controller->index(new Request(), $useCase);
+        $response = $this->controller->index(new Request(), $useCase);
 
         $this->assertInstanceOf(AnonymousResourceCollection::class, $response);
         $this->assertArrayHasKey('meta', $response->additional);
+    }
+
+    public function testStore(): void
+    {
+        $useCase = new CreateCategoryUseCase($this->repository);
+
+        $request = new StoreCategoryRequest();
+        $request->headers->set('Content-type', 'application/json');
+        $request->setJson(new InputBag([
+            'name' => 'Teste'
+        ]));
+
+        $response = $this->controller->store($request, $useCase);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_CREATED, $response->status());
     }
 }
