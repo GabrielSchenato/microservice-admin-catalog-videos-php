6507eb27da709f53f1e329cc4dbabc9ddf2f2a82
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-09 15:54:46 -0300

    Category Controller Feature Test

--- tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
@@ -8,11 +8,11 @@ use App\Models\Category;
 use App\Repositories\Eloquent\CategoryEloquentRepository;
 use Core\UseCase\Category\CreateCategoryUseCase;
 use Core\UseCase\Category\ListCategoriesUseCase;
+use Core\UseCase\Category\ListCategoryUseCase;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Symfony\Component\HttpFoundation\InputBag;
-use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
@@ -52,5 +52,18 @@ class CategoryControllerTest extends TestCase
 
         $this->assertInstanceOf(JsonResponse::class, $response);
         $this->assertEquals(Response::HTTP_CREATED, $response->status());
+    }
+
+    public function testShow(): void
+    {
+        $category = Category::factory()->create();
+
+        $response = $this->controller->show(
+            id: $category->id,
+            useCase: new ListCategoryUseCase($this->repository)
+        );
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_OK, $response->status());
     }
 }
