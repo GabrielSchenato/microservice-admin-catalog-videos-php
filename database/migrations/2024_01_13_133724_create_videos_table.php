20a3c6ec1d1d95a4d4b0a5b62cfe4b15abc252c8
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-13 11:00:05 -0300

    Migration Videos

--- database/migrations/2024_01_13_133724_create_videos_table.php
+++ database/migrations/2024_01_13_133724_create_videos_table.php
@@ -4,15 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('videos', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->text('description');
+            $table->smallInteger('year_launched');
+            $table->boolean('opened')->default(false);
+            $table->string('rating', 3);
+            $table->smallInteger('duration');
+            // $table->string('thumb_file')->nullable();
+            // $table->string('thumb_half_file')->nullable();
+            // $table->string('banner_file')->nullable();
+            // $table->string('trailer_file')->nullable();
+            // $table->string('video_file')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
