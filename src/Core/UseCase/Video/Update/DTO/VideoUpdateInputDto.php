7c80658860c9801c6693c9a36cda118e142d68f2
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-13 09:21:10 -0300

    Unit tests Update Use Case Video DTO

--- src/Core/UseCase/Video/Update/DTO/VideoUpdateInputDto.php
+++ src/Core/UseCase/Video/Update/DTO/VideoUpdateInputDto.php
@@ -2,18 +2,13 @@
 
 namespace Core\UseCase\Video\Update\DTO;
 
-use Core\Domain\Enum\Rating;
-
 class VideoUpdateInputDto
 {
     public function __construct(
+        public string $id,
         public string $title,
         public string $description,
-        public int    $yearLaunched,
-        public int    $duration,
-        public bool   $opened,
-        public Rating $rating,
-        public array  $categoriesId ,
+        public array  $categoriesId,
         public array  $genresId,
         public array  $castMembersId,
         public ?array $videoFile = null,
