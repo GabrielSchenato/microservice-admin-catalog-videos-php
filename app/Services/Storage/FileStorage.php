3f1693d444591dbfca12bb6fc63df384b322ba7c
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-15 08:35:23 -0300

    FileStorage implementation

--- app/Services/Storage/FileStorage.php
+++ app/Services/Storage/FileStorage.php
@@ -3,17 +3,31 @@
 namespace App\Services\Storage;
 
 use Core\UseCase\Interfaces\FileStorageInterface;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class FileStorage implements FileStorageInterface
 {
 
     public function store(string $path, array $file): string
     {
-        return '';
+        $contents = $this->convertFileToLaravelFile($file);
+
+        return Storage::put($path, $contents);
     }
 
     public function delete(string $path): bool
     {
-        return true;
+        return Storage::delete($path);
+    }
+
+    protected function convertFileToLaravelFile(array $file): UploadedFile
+    {
+        return new UploadedFile(
+            path: $file['tmp_name'],
+            originalName: $file['name'],
+            mimeType: $file['type'],
+            error: $file['error']
+        );
     }
 }
