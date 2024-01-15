738831fc325fbd30b75ac5545f6b85154fd77ffa
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-15 19:19:51 -0300

    Consumer

--- app/Services/AMQP/PhpAmqpService.php
+++ app/Services/AMQP/PhpAmqpService.php
@@ -14,31 +14,25 @@ class PhpAmqpService implements AMQPInterface
     protected ?AMQPStreamConnection $connection = null;
     protected AbstractChannel|null|AMQPChannel $channel = null;
 
-    public function __construct()
-    {
-        if ($this->connection) {
-            return;
-        }
-
-        $configs = config('microservices.rabbitmq.hosts')[0];
-        $this->connection = new AMQPStreamConnection(
-            host: $configs['host'],
-            port: $configs['port'],
-            user: $configs['user'],
-            password: $configs['password'],
-            vhost: $configs['vhost']
-        );
-
-        $this->channel = $this->connection->channel();
-    }
-
     public function producer(string $queue, array $payload, string $exchange): void
     {
+        $this->connect();
 
+        $this->channel->queue_declare($queue, false, true, false, false);
+        $this->channel->exchange_declare($exchange, AMQPExchangeType::DIRECT, false, true, false);
+        $this->channel->queue_bind($queue, $exchange);
+
+        $message = new AMQPMessage(json_encode($payload), ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]);
+        $this->channel->basic_publish($message, $exchange);
+
+        $this->closeChannel();
+        $this->closeConnection();
     }
 
     public function producerFanout(array $payload, string $exchange): void
     {
+        $this->connect();
+
         $this->channel->exchange_declare(
             exchange: $exchange,
             type: AMQPExchangeType::FANOUT,
@@ -58,7 +52,49 @@ class PhpAmqpService implements AMQPInterface
 
     public function consumer(string $queue, string $exchange, Closure $callback): void
     {
+        $this->connect();
 
+        $this->channel->queue_declare(
+            queue: $queue,
+            durable: true,
+            auto_delete: false
+        );
+
+        $this->channel->queue_bind(
+            queue: $queue,
+            exchange: $exchange,
+            routing_key: config('microservices.queue_name')
+        );
+
+        $this->channel->basic_consume(
+            queue: $queue,
+            callback: $callback
+        );
+
+        while ($this->channel->is_consuming()) {
+            $this->channel->wait();
+        }
+
+        $this->closeChannel();
+        $this->closeConnection();
+    }
+
+    private function connect(): void
+    {
+        if ($this->connection) {
+            return;
+        }
+
+        $configs = config('microservices.rabbitmq.hosts')[0];
+        $this->connection = new AMQPStreamConnection(
+            host: $configs['host'],
+            port: $configs['port'],
+            user: $configs['user'],
+            password: $configs['password'],
+            vhost: $configs['vhost']
+        );
+
+        $this->channel = $this->connection->channel();
     }
 
     private function closeChannel(): void
