93ebe280a7ab99110560f5b19e1ab0c001b58fc8
Gabriel Schenato <gabrielschenato152@hotmail.com>
2024-01-15 10:45:35 -0300

    List Video Use Case Integration tests

---
+++ tests/Feature/Core/UseCase/Video/ListVideoUseCaseTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Video;
+
+use App\Models\Video;
+use Core\Domain\Exception\NotFoundException;
+use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\UseCase\Video\List\DTO\VideoListInputDto;
+use Core\UseCase\Video\List\ListVideoUseCase;
+use Tests\TestCase;
+
+
+class ListVideoUseCaseTest extends TestCase
+{
+
+    public function testList(): void
+    {
+        $video = Video::factory()->create();
+
+        $useCase = new ListVideoUseCase(
+            $this->app->make(VideoRepositoryInterface::class)
+        );
+
+        $response = $useCase->execute(new VideoListInputDto(
+            id: $video->id
+        ));
+
+        $this->assertNotNull($response);
+        $this->assertEquals($video->id, $response->id);
+    }
+
+    public function testListIdNotFound()
+    {
+        $this->expectException(NotFoundException::class);
+
+        $useCase = new ListVideoUseCase(
+            $this->app->make(VideoRepositoryInterface::class)
+        );
+        $useCase->execute(new VideoListInputDto(
+            id: 'fake_id'
+        ));
+    }
+
+}
